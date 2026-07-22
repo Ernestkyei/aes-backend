45256068857f235915bc275e4a0184927d8ab40d
ErnestKyei <kyeiernest86@gmail.com>
2026-07-22 10:32:03 +0000

    backendconfig

---
+++ server.js
@@ -0,0 +1,15 @@
+const app  = require("./app");
+const dotenv = require("dotenv");
+
+
+
+dotenv.config({path: "./config/config.env"});
+const PORT = process.env.PORT
+
+
+
+
+
+app.listen(PORT, () =>{
+    console.log(`server is running on port ${PORT}`);
+})
