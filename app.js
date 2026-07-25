5b0b3d1d34f32d960f5c5520b5f5c18bd1cd7d88
ErnestKyei <kyeiernest86@gmail.com>
2026-07-25 19:37:16 +0000

    models done

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-//=========http morgan request==================
+//=========http morgan request================
 app.use(morgan("dev"));
 
 
